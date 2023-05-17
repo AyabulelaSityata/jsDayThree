e311293b36867c6860a1eb19c1cf2dbbbdb19562
AyabulelaSityata <sityataayabulela@gmail.com>
2023-05-17 15:25:47 +0200

    day three

---
+++ script/code.js
@@ -0,0 +1,15 @@
+// Exercise Day Three: Question 1
+/*var subjects = 'mathematics'
+subjects = `25`
+console.log(subjects)
+*/
+// Question 2
+
+// var subjects = 25
+// console.log(subjects)
+
+// Yes it changed
+
+// Question 3
+var dataTypes = [`Aya`,`25`,`True`,`<=`,`undefined`,`null`];
+console.log(typeof(dataTypes));
