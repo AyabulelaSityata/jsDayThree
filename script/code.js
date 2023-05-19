4b7e41ed266395ab3b1cd44a125e7349d3218396
AyabulelaSityata <sityataayabulela@gmail.com>
2023-05-19 09:47:33 +0200

    reattempt

--- script/code.js
+++ script/code.js
@@ -1,15 +1,34 @@
-// Exercise Day Three: Question 1
-/*var subjects = 'mathematics'
-subjects = `25`
-console.log(subjects)
+/*Exercise Day Three:
+Question 1
+var subjects = 'mathematics';
+console.log(typeof subjects);
+string
+
+Question 2
+var subjects = 25;
+console.log(typeof subjects)
+typeof number
+
+Yes it changed
+
+Question 3
+var schoolName = "Sinako High Shool";
+console.(typeof schoolName);
+string
+
+var myGrade = 12;
+console.log(typeof myGrade);
+number
+
+var passedMatric = true;
+console.log(typeof passedMatric)
+boolean
+
+var lastName;
+console.log(typeof lastName);
+undefined
+
+var shoeSize = null;
+console.log(typeof shoeSize);
+object
 */
-// Question 2
-
-// var subjects = 25
-// console.log(subjects)
-
-// Yes it changed
-
-// Question 3
-var dataTypes = [`Aya`,`25`,`True`,`<=`,`undefined`,`null`];
-console.log(typeof(dataTypes));
